366d4e6b02dd80b827fd73f448c5f35868822745
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-05-11 09:23:52 +0300

    add pages

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// const NotFoundPage = () => {
+// 	return <Navigate to="/" />;
+// };
+
+// export default NotFoundPage;
