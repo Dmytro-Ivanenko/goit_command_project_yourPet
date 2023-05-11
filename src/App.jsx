b98ec0727ee6af7db1cd3735bde03095f46b630b
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-05-12 01:16:15 +0300

    fix sharedLayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 
 import UserRoutes from './UserRoutes';
 
-import styles from './sass/base.module.scss';
-
 export const App = () => {
-  return (
-    <div className={styles.container}>
-      <UserRoutes />
-    </div>
-  );
+    return <UserRoutes />;
 };
