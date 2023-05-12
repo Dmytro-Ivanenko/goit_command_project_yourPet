b925867c7f60918126a6985e3d81952ad4c7291f
supirgigi <107944673+supirgigi@users.noreply.github.com>
2023-05-12 16:47:25 +0300

    fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
 import './index.scss';
 
+import '@fontsource/manrope';
+import '@fontsource/inter';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter basename="/goit_command_project_yourPet">
