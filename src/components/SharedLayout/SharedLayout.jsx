b2fa4d36ba4790e40656a5f1d9f271927efb991a
Ernes Nafieiev <demodekoz@gmail.com>
2023-05-13 00:43:33 +0300

    add

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from '../Header/Header';
 import styles from './sharedLayout.module.scss';
 import UserRoutes from 'routes/UserRoutes';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 
 const SharedLayout = () => {
     return (
         <div className={styles.container}>
             <Header />
             <UserRoutes />
+            <NotFoundPage />
         </div>
     );
 };
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,3 +5,22 @@
 // };
 
 // export default NotFoundPage;
+import { Link } from 'react-router-dom';
+
+import styles from './not-found-page.module.scss';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1 className={styles.title}>Ooops! This page not found :( </h1>
+            <p className={styles.digit}>
+                <span className={styles.digit_right}>4</span> 0 <span className={styles.digit_left}>4</span>
+            </p>
+            <Link to="/" className={styles.btn}>
+                To main page
+            </Link>
+        </div>
+    );
+};
+
+export default NotFoundPage;
