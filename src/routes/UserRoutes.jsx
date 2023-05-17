c70b8afaad17b5dcf17ad1dc12b32c217d700c1b
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-05-17 10:37:38 +0300

    Update UserRoutes.jsx

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -8,7 +8,7 @@ const MainPage = lazy(() => import('pages/MainPage/MainPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
 const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 const AddPetPage = lazy(() => import('pages/AddPetPage/AddPetPage'));
-// const UserPage = lazy(() => import('pages/UserPage/UserPage'));
+const UserPage = lazy(() => import('pages/UserPage/UserPage'));
 const NoticesPage = lazy(() => import('pages/NoticesPage'));
 // const OurFriendsPage = lazy(() => import('pages/OurFriendsPage/OurFriendsPage'));
 // const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
@@ -25,13 +25,13 @@ const UserRoutes = () => {
                 {/* <Route path="/news" element={<NewsPage />} /> */}
                 <Route element={<PublicRoute />}>
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    
+                    <Route path="/register" element={<RegisterPage />} />  
                 </Route>
-                {<Route element={<PrivateRoute />}>
-                    {/* <Route path="/user" element={<UserPage />} /> */}
+
+               {/* {<Route element={<PrivateRoute />}>*/}
+                     <Route path="/user" element={<UserPage />} /> 
                     <Route path="/add-pet" element={<AddPetPage />} />
-                </Route>}
+                {/*  </Route> */}
 
                 <Route path="/notices" element={<NoticesPage />}>
                     <Route index element={<Navigate to="/notices/sell" replace />} />
