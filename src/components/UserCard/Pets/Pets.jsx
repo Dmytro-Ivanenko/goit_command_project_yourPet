3b87a0309e588382af1c7e0efdcb7a960eac8f9f
supirgigi <107944673+supirgigi@users.noreply.github.com>
2023-05-20 19:18:24 +0300

    adds api logic to pets on user page

--- src/components/UserCard/Pets/Pets.jsx
+++ src/components/UserCard/Pets/Pets.jsx
@@ -1,60 +1,14 @@
-import styles from './pets.module.scss';
-import cat from '../../../images/cat.jpeg';
-import trash from '../../../images/icons/trash.svg';
-import dog from '../../../images/dog.jpeg';
+import PetsItem from './PetsItem';
 
-const Pets = () => {
+import styles from './pets.module.scss';
+
+const Pets = ({ pets }) => {
     return (
-        <div>
-            <div className={styles.container}>
-                <img className={styles.photoPet} src={cat} alt="cat" />
-                <div className={styles.wrapData}>
-                    <p className={styles.cardTitle}>
-                        <p className={styles.text}>
-                            <span className={styles.title}>Name: </span>Jack
-                        </p>
-                        <button type="submit" className={styles.btnTrash}>
-                            <img className={styles.icon} src={trash} width="24" height="24" alt="trash" />
-                        </button>
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Date of birth: </span>22.04.2018
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Breed: </span>Persian cat
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Comments: </span>Jack is a gray Persian cat with green eyes. He
-                        loves to be pampered and groomed, and enjoys playing with toys. Although a bitshy, he's a loyal
-                        and affectionate lap cat.
-                    </p>
-                </div>
-            </div>
-            <div className={styles.container}>
-                <img className={styles.photoPet} src={dog} width="240" height="240" alt="dog" />
-                <div className={styles.wrapData}>
-                    <p className={styles.cardTitle}>
-                        <p className={styles.text}>
-                            <span className={styles.title}>Name: </span>Jack
-                        </p>
-                        <button type="submit" className={styles.btnTrash}>
-                            <img className={styles.icon} src={trash} width="24" height="24" alt="trash" />
-                        </button>
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Date of birth: </span>22.04.2018
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Breed: </span>Persian cat
-                    </p>
-                    <p className={styles.text}>
-                        <span className={styles.title}>Comments: </span>Jack is a gray Persian cat with green eyes. He
-                        loves to be pampered and groomed, and enjoys playing with toys. Although a bitshy, he's a loyal
-                        and affectionate lap cat.
-                    </p>
-                </div>
-            </div>
-        </div>
+        <ul className={styles.list}>
+            {pets.map(item => (
+                <PetsItem key={item._id} item={item} />
+            ))}
+        </ul>
     );
 };
 
---
+++ src/pages/UserPage/items.js
@@ -0,0 +1,22 @@
+import cat from 'images/example/cat.jpg';
+
+const items = [
+    {
+        name: 'Cat',
+        birthDate: '21-10-2022',
+        breed: 'breed',
+        comments:
+            'Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys. Although a bitshy, hes a loyal and affectionate lap cat.',
+        photoURL: cat,
+    },
+    {
+        name: 'Cat',
+        birthDate: '21-10-2022',
+        breed: 'breed',
+        comments:
+            'Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys. Although a bitshy, hes a loyal and affectionate lap cat.',
+        photoURL: cat,
+    },
+];
+
+export default items;
