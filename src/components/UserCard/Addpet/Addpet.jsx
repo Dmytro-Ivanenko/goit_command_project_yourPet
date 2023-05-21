77f95a145e61c66291980c5031e8503f21b56e00
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-05-21 22:18:49 +0300

    Update Addpet.jsx

--- src/components/UserCard/Addpet/Addpet.jsx
+++ src/components/UserCard/Addpet/Addpet.jsx
@@ -1,5 +1,5 @@
 import styles from './addpet.module.scss';
-import { ReactComponent as PlusSmallIcon } from 'images/icons/plus-small.svg';
+//import { ReactComponent as PlusSmallIcon } from 'images/icons/plus-small.svg';
 
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
@@ -12,7 +12,7 @@ const Addpet = () => {
             <Link to="/add-pet" state={{ from: location }} className={styles.link}>
                 <button className={styles.addPet}>
                     <span className={styles.titleBtn}>Add Pet</span>
-                    <img src={plus} width="24" height="24" alt="plus" />
+                    //<img src={plus} width="24" height="24" alt="plus" />
                 </button>
             </Link>
         </div>
