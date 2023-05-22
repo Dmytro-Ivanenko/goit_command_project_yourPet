3c725152bb78b7f8df6e258af80c5ddfcc7e1a97
supirgigi <107944673+supirgigi@users.noreply.github.com>
2023-05-22 19:15:54 +0300

    reusable button

--- src/components/ModalCongrats/ModalCongrats.jsx
+++ src/components/ModalCongrats/ModalCongrats.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
-import ModalApproveAction from 'shared/components/ModalApproveAction';
 
-import { ReactComponent as PawprintIcon } from 'images/icons/pawprint.svg';
+import ModalApproveAction from 'shared/components/ModalApproveAction';
+import Button from 'shared/components/Button';
 
 import styles from './ModalCongrats.module.scss';
 
@@ -11,10 +11,7 @@ const ModalCongrats = ({ onClose }) => {
             <div className={styles.container}>
                 <h3 className={styles.title}>Congrats!</h3>
                 <p className={styles.text}>Your registration is successful.</p>
-                <button className={styles.btnLearn} onClick={onClose}>
-                    <span className={styles.btnLearnText}> Go to profile</span>
-                    <PawprintIcon className={styles.btnLearnIcon} width={24} height={24} />
-                </button>
+                <Button onClick={onClose} text="Go to profile" />
             </div>
         </ModalApproveAction>
     );
--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -4,18 +4,18 @@ import styles from './button.module.scss';
 // Icons
 import { ReactComponent as PawprintIcon } from 'images/icons/pawprint.svg';
 
-const Button = ({ onClose, btnName }) => {
+const Button = ({ onClick, text }) => {
     return (
-        <button className={styles.btnLearn} onClick={onClose}>
-            <span className={styles.btnLearnText}>{btnName}</span>
+        <button className={styles.btnLearn} onClick={onClick}>
+            <span className={styles.btnLearnText}>{text}</span>
             <PawprintIcon className={styles.btnLearnIcon} width={24} height={24} />
         </button>
     );
 };
 
 Button.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    btnName: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    text: PropTypes.string.isRequired,
 };
 
 export default Button;
