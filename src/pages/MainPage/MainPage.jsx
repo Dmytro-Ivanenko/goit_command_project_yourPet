bd2d572bb3da57a7d81fe80d8c6376521b273f19
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-05-22 18:26:32 +0300

    Attampt4

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 import desktop from '../../images/mainPage-pictures-desktop.jpg';
-import mobile from '../../images/mainPage-pictures-mobile.jpg';
-import tablet from '../../images/mainPage-pictures-tablet.jpg';
+import desktop1xjpg from '../../images/PictureMainPage-desktop1xjpg.jpg';
+import desktop3xjpg from '../../images/PictureMainPage-desktop3xjpg.jpg';
+import desktop1xwebp from '../../images/PictureMainPage-desktop1xwebp.webp';
+import desktop3xwebp from '../../images/PictureMainPage-desktop3xwebp.webp';
+// import mobile from '../../images/mainPage-pictures-mobile.jpg';
+import mobile1xjpg from '../../images/PictureMainPage-mobile1xjpg.jpg';
+import mobile2xjpg from '../../images/PictureMainPage-mobile2xjpg.jpg';
+import mobile1xwebp from '../../images/PictureMainPage-mobile1xwebp.webp';
+import mobile2xwebp from '../../images/PictureMainPage-mobile2xwebp.webp';
+// import tablet from '../../images/mainPage-pictures-tablet.jpg';
+import tablet1xjpg from '../../images/PictureMainPage-tablet1xjpg.jpg';
+import tablet2xjpg from '../../images/PictureMainPage-tablet2xjpg.jpg';
+import tablet1xwebp from '../../images/PictureMainPage-tablet1xwebp.webp';
+import tablet2xwebp from '../../images/PictureMainPage-tablet2xwebp.webp';
 
 import styles from './MainPage.module.scss';
 
@@ -13,9 +25,16 @@ const MainPage = () => {
                 <h1 className={styles.mainPageText}>Take good care of your small pets</h1>
 
                 <picture>
-                    <source srcSet={mobile} media="(max-width: 767px)" />
-                    <source srcSet={tablet} media="(min-width: 768px) and (max-width: 1279px)" />
-                    <source srcSet={desktop} media="(min-width: 1280px)" />
+                    <source srcSet={(desktop1xwebp, desktop3xwebp)} media="(min-width: 1280px)" />
+                    <source srcSet={(desktop1xjpg, desktop3xjpg)} media="(min-width: 1280px)" />
+                    <source srcSet={(tablet1xwebp, tablet2xwebp)} media="(min-width: 768px) and (max-width: 1279px)" />
+                    <source srcSet={(tablet1xjpg, tablet2xjpg)} media="(min-width: 768px) and (max-width: 1279px)" />
+                    <source srcSet={(mobile1xwebp, mobile2xwebp)} media="(max-width: 767px)" />
+                    <source srcSet={(mobile1xjpg, mobile2xjpg)} media="(max-width: 767px)" />
+
+                    {/* <source srcSet={mobile} media="(max-width: 767px)" /> */}
+                    {/* <source srcSet={tablet} media="(min-width: 768px) and (max-width: 1279px)" /> */}
+                    {/* <source srcSet={desktop} media="(min-width: 1280px)" /> */}
                     <img className={styles.imgMainPage} src={desktop} alt="Cute puppy" />
                 </picture>
             </div>
