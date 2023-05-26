bca3cd07eec16e9082ff249d817540904e10baff
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-05-26 17:33:40 +0300

    d

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import Pagination from '../../shared/components/Pagination';
 import NewsItem from './NewsItem';
-import newsSource from './newsSource';
 import styles from './news_list.module.scss';
 
 const PER_PAGE = 12;
@@ -11,136 +10,47 @@ const PER_PAGE = 12;
 const NewsList = ({ list }) => {
     const [items, setItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
-    const [currentPage, setCurrentPage] = useState(0);
-    // const [currentItems, setCurrentItems] = useState([]);
-    // const [pageCount, setPageCount] = useState(0);
-    // const [itemOffset, setItemOffset] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         setPageCount(Math.ceil(list.length / PER_PAGE));
         const startOffset = (currentPage * PER_PAGE) % list.length;
         const endOffset = startOffset + PER_PAGE;
         const paginatedNews = list.slice(startOffset, endOffset);
-
         setItems(paginatedNews);
-        // const endOffset = itemOffset + PER_PAGE;
+    }, [currentPage, list]);
 
-        // setItemOffset(0);
-        // setCurrentItems(items.slice(itemOffset, endOffset));
-        // setPageCount(1);
-        // return;
-
-        // const endOffset = itemOffset + PER_PAGE;
-        // setCurrentItems(items.slice(itemOffset, endOffset));
-        // setPageCount(Math.ceil(items.length / PER_PAGE));
-    }, [currentPage]);
-
-    // const onPageClick = useCallback(
-    //     event => {
-    //         const newOffset = (event.selected * PER_PAGE) % items.length;
-    //         setItemOffset(newOffset);
-    //     },
-    //     [items.length]
-    // );
-
+    const onPageClick = useCallback(event => {
+        setCurrentPage(event.selected + 1);
+    }, []);
     return (
         <>
-            <ul className={styles.list}>
-                {Boolean(items.length)
-                    ? items.map(item => <NewsItem key={item.id} item={item} />)
-                    : list.map(item => <NewsItem key={item.id} item={item} />)}
-            </ul>
-            {pageCount > 1 && <Pagination pageCount={pageCount} />}
+            {items.length > 0 && (
+                <ul className={styles.list}>
+                    {items.map(item => (
+                        <NewsItem key={item._id} item={item} />
+                    ))}
+                </ul>
+            )}
+            {pageCount > 1 && <Pagination pageCount={pageCount} onPageClick={onPageClick} currentPage={currentPage} />}
         </>
     );
 };
 
-// NewsList.defaultProps = {
-//     list: [],
-// };
+NewsList.defaultProps = {
+    list: [],
+};
 
-// NewsList.propTypes = {
-//     list: PropTypes.arrayOf(
-//         PropTypes.shape({
-//             url: PropTypes.string,
-//             text: PropTypes.string.isRequired,
-//             title: PropTypes.string.isRequired,
-//             imgUrl: PropTypes.string.isRequired,
-//             date: PropTypes.string.isRequired,
-//         }).isRequired
-//     ),
-// };
+NewsList.propTypes = {
+    list: PropTypes.arrayOf(
+        PropTypes.shape({
+            url: PropTypes.string,
+            text: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            imgUrl: PropTypes.string.isRequired,
+            date: PropTypes.string.isRequired,
+        }).isRequired
+    ),
+};
 
 export default NewsList;
-
-// import { useEffect, useState, useCallback } from 'react';
-// import PropTypes from 'prop-types';
-
-// import Pagination from '../../shared/components/Pagination';
-// import NewsItem from './NewsItem';
-// import newsSource from './newsSource';
-// import styles from './news_list.module.scss';
-
-// const PER_PAGE = 12;
-
-// const NewsList = ({ list }) => {
-//     const [items, setItems] = useState(newsSource);
-//     const [currentItems, setCurrentItems] = useState([]);
-//     const [pageCount, setPageCount] = useState(0);
-//     const [itemOffset, setItemOffset] = useState(0);
-
-//     useEffect(() => {
-//         if (list.length !== 0) {
-//             setItems(list);
-//             const endOffset = itemOffset + PER_PAGE;
-
-//             setItemOffset(0);
-//             setCurrentItems(items.slice(itemOffset, endOffset));
-//             setPageCount(1);
-//             return;
-//         } else {
-//             setItems(newsSource);
-//         }
-
-//         const endOffset = itemOffset + PER_PAGE;
-//         setCurrentItems(items.slice(itemOffset, endOffset));
-//         setPageCount(Math.ceil(items.length / PER_PAGE));
-//     }, [itemOffset, items, list, pageCount]);
-
-//     const onPageClick = useCallback(
-//         event => {
-//             const newOffset = (event.selected * PER_PAGE) % items.length;
-//             setItemOffset(newOffset);
-//         },
-//         [items.length]
-//     );
-
-//     return (
-//         <>
-//             <ul className={styles.list}>
-//                 {currentItems.map(item => (
-//                     <NewsItem key={item.id} item={item} />
-//                 ))}
-//             </ul>
-//             {pageCount > 1 && <Pagination onPageClick={onPageClick} pageCount={pageCount} />}
-//         </>
-//     );
-// };
-
-// NewsList.defaultProps = {
-//     list: [],
-// };
-
-// NewsList.propTypes = {
-//     list: PropTypes.arrayOf(
-//         PropTypes.shape({
-//             url: PropTypes.string,
-//             text: PropTypes.string.isRequired,
-//             title: PropTypes.string.isRequired,
-//             imgUrl: PropTypes.string.isRequired,
-//             date: PropTypes.string.isRequired,
-//         }).isRequired
-//     ),
-// };
-
-// export default NewsList;
--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import PageTitle from 'shared/components/PageTitle';
 import SearchForm from 'shared/components/SearchForm';
-// import newsSource from '../../components/NewsList/newsSource.json';
 import NewsList from '../../components/NewsList';
 import styles from './news_page.module.scss';
 import { ToastContainer, toast } from 'react-toastify';
@@ -11,18 +10,19 @@ import sendRequest from 'components/NewsList/sendRequest';
 const NewsPage = () => {
     const [list, setList] = useState([]);
     const [currentList, setCurrentList] = useState([]);
+
     useEffect(() => {
-        const requestHandler = async () => {
+        (async () => {
             const newsArray = await sendRequest();
             setList(newsArray);
-        };
-        requestHandler();
+        })();
     }, []);
 
     const searchNewsByInputWord = ({ query }) => {
         const upperedWord = query[0].toUpperCase() + query.slice(1);
         const loweredWord = query[0].toLowerCase() + query.slice(1);
         let findOne = false;
+
         for (const newsObj of list) {
             const { title, text } = newsObj;
             console.log(title);
@@ -32,22 +32,17 @@ const NewsPage = () => {
                 text.includes(upperedWord) ||
                 text.includes(loweredWord)
             ) {
-                console.log('FOUND TITLE:', title);
-                // if (list.includes(newsObj)) return;
                 findOne = true;
                 return setCurrentList(prev => [...prev, newsObj]);
-                // return setList(prevState => [...prevState, newsObj]);
             }
         }
+
         if (!findOne) {
             toast.warn('No such news!', {
                 position: 'top-right',
                 autoClose: 3000,
                 theme: 'colored',
             });
-
-            // setList([]);
-            // return;
         }
     };
 
