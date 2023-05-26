1ba71df51f4c4d423633d564cc73c580aeccbf80
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2023-05-26 21:09:22 +0300

    f

--- src/components/NewsList/sendRequest.jsx
+++ src/components/NewsList/sendRequest.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const sendRequest = async () => {
     try {
         let response = await axios.get('/news/');
-        console.log(response.data);
+
         return response.data;
     } catch (err) {
         console.log(err);
